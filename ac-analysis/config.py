a3169d7a7f4dd4e636ae7ca7f41f8ab26c4af574
Johan Pauwels <jpauwels@users.noreply.github.com>
2018-11-07 17:41:23 +0000

    Add Europeana as content provider

--- ac-analysis/config.py
+++ ac-analysis/config.py
@@ -10,6 +10,10 @@ def audio_uri(file_id, provider):
         r = requests.get('https://freesound.org/apiv2/sounds/{id}/'.format(id=file_id), params={'token': os.getenv('FREESOUND_API_KEY'), 'fields': 'previews'})
         return r.json()['previews']['preview-hq-ogg']
     elif provider == 'europeana':
-        return 'http://{}'.format(file_id)
+        r = requests.get('http://www.europeana.eu/api/v2/record/{id}.json'.format(id=file_id), params={'wskey': os.getenv('EUROPEANA_API_KEY')})
+        if r.status_code == requests.codes['ok'] and r.json()['success']:
+            return r.json()['object']['aggregations'][0]['edmIsShownBy']
+        else:
+            raise  ValueError('The audio file for id "{}" could not be retrieved from Europeana'.format(file_id))
     else:
         raise ValueError('Unknown audio provider "{}"'.format(provider))
