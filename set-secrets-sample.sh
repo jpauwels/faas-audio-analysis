509b9408197cadc7e9310c08af22cd37f008272e
Johan Pauwels <jpauwels@users.noreply.github.com>
2024-06-12 13:24:06 +0100

    Align demo secrets with k8s-storage repo

--- set-secrets-sample.sh
+++ set-secrets-sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export OPENFAAS_URL=$(perl -ne 'print "$1\n" if /gateway: (.*)$/' stack.yml)
-faas secret create database-connection --from-literal=mongodb://localhost:27017/test
+faas secret create database-connection --from-literal="mongodb://admin:test@mongo.storage.svc.cluster.local:27017/?directConnection=true&authSource=admin"
 faas secret create freesound-api-key --from-literal=secret-string
 faas secret create europeana-api-key --from-literal=another-secret-string
 faas secret create object-store-access-key --from-literal=access-key
